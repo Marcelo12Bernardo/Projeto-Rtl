7c251210fe76caba2c05775796efe756b3f15865
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2023-01-20 14:48:56 -0300

    Iniciando projeto

--- src/index.js
+++ src/index.js
@@ -14,4 +14,5 @@ createRoot(document.getElementById('root')).render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+// Iniciando projeto
 serviceWorker.unregister();
